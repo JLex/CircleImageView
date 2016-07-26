5ae41eb6a4b12fcc2c4c5b0e295fadc9fed0c1e5
JLex <joe_xd7@hotmail.com>
2016-07-26 11:44:32 -0500

    Prueba1

--- app/src/main/java/jlexdev/com/customview/MainActivity.java
+++ app/src/main/java/jlexdev/com/customview/MainActivity.java
@@ -3,6 +3,12 @@ package jlexdev.com.customview;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/** CIRCLE IMAGEVIEW
+ *
+ *  Fuente:
+ *
+ *  https://github.com/hdodenhof/CircleImageView
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
